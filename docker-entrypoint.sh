0220f1087b1580af8d4d3c17e373a75de261e157
Coco Pettoruti <coco@Cocos-MacBook-Pro.local>
2018-06-22 01:28:36 +0100

    Now adding the files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -e
 
-until psql $DATABASE_URL -c '\l'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
+if [ ! -z "$DATABASE_URL" ]; then
+    until psql $DATABASE_URL -c '\l'; do
+      >&2 echo "Postgres is unavailable - sleeping"
+      sleep 1
+    done
+fi
 
 >&2 echo "Postgres is up - continuing"
 
@@ -14,6 +16,12 @@ fi
 
 if [ "x$DJANGO_MANAGEPY_COLLECTSTATIC" = 'xon' ]; then
     /venv/bin/python manage.py collectstatic --noinput
+fi
+
+# CREATE_SUPER_USER=admin:admin@example.com:password
+if [[ -n "$CREATE_SUPER_USER" ]]; then
+    echo "==> Creating super user"
+    printf "from django.contrib.auth.models import User\nif not User.objects.exists(): User.objects.create_superuser(*'$CREATE_SUPER_USER'.split(':'))" | /venv/bin/python manage.py shell
 fi
 
 exec "$@"
